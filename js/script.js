96af8b09795f1a3161808b16cb8cde8e8fd7c438
omarjafor <omarjaforchy@gmail.com>
2022-08-23 13:11:41 +0600

    new update

--- js/script.js
+++ js/script.js
@@ -146,24 +146,24 @@
 //     document.write("Zero");
 
 //Task Number Four code for student grade mark -------->
-// var num = prompt("Enter your mark number: ");
-// num = parseInt(num);
+var num = prompt("Enter your mark number: ");
+num = parseInt(num);
 
-// if(num > 100 || num < 0)
-//     document.write("Invalid Number");
-// else if(num >= 80 && num <= 100)
-//     document.write("A+");
-// else if(num >= 70 && num <= 79)
-//     document.write("A");
-// else if(num >= 60 && num <= 69)
-//     document.write("A-");
-// else if(num >= 50 && num <= 59)
-//     document.write("B");
-// else if(num >= 40 && num <= 49)
-//     document.write("C");
-// else if(num >= 33 && num <= 39)
-//     document.write("D");
-// else
-//     document.write("Fail");
+if(num > 100 || num < 0)
+    document.write("Invalid Number");
+else if(num >= 80 && num <= 100)
+    document.write("A+");
+else if(num >= 70 && num <= 79)
+    document.write("A");
+else if(num >= 60 && num <= 69)
+    document.write("A-");
+else if(num >= 50 && num <= 59)
+    document.write("B");
+else if(num >= 40 && num <= 49)
+    document.write("C");
+else if(num >= 33 && num <= 39)
+    document.write("D");
+else
+    document.write("Fail");
 
 //New Program code for student grade mark -------->
