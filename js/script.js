008091f21ae79cf26fa6fff1ad400c7c65408320
omarjafor <omarjaforchy@gmail.com>
2022-09-10 22:27:03 +0600

    Image Slider

--- js/script.js
+++ js/script.js
@@ -892,11 +892,24 @@
 var photos = ["images/couple.jpg", "images/jabed.jpg", "images/shantu.jpg"];
 var imgTag = document.querySelector("img");
 
-count = 0;
-function prev(){
-
-}
+var count = 0;
 
 function next(){
+    count++;
+    if(count>=photos.length){
+        count = 0;
+        imgTag.src = photos[count];
+    }else{
+        imgTag.src = photos[count];
+    }
+}
 
+function prev(){
+    count--;
+    if(count < 0){
+        count = photos.length - 1;
+        imgTag.src = photos[count];
+    }else{
+        imgTag.src = photos[count];
+    }
 }
