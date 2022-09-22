62cd2be8acd7c4612e5c4b2a7ef82bfb9c6a6040
omarjafor <omarjaforchy@gmail.com>
2022-09-22 23:24:04 +0600

    port loc

--- js/script.js
+++ js/script.js
@@ -1240,7 +1240,7 @@
 // });
 // New Program Code For Bom Event - Browser Object Model ------------------>
 // console.log(window);
-// console.log(location.href);
+console.log(location.href);
 console.log(location.protocol);
 console.log(location.port);
 console.log(location.host);
