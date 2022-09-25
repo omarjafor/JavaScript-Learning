28da6bcb4178c025c58886afc62846bf3bfbd7de
omarjafor <omarjaforchy@gmail.com>
2022-09-25 10:54:23 +0600

    confirm box

--- js/script.js
+++ js/script.js
@@ -1274,3 +1274,17 @@
 // }
 // deleteSomething();
 // Prompt Box --------->
+function welcomeMessage(){
+    var h1 = document.createElement('h1');
+    let text;
+    var name = prompt("Enter Your Name : ");
+    if (name == null || name == ""){
+        text = 'No name found';
+    }else{
+        text = "Welcome " + name;
+    }
+    var textNode = document.createTextNode(text);
+    h1.appendChild(textNode);
+    document.body.appendChild(h1);
+}
+welcomeMessage();
