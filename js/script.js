2a3ce6cbe0a900f046b5f2f8c852820169b5f4a6
omarjafor <omarjaforchy@gmail.com>
2022-09-26 11:28:11 +0600

    javascript clock

--- js/script.js
+++ js/script.js
@@ -1321,3 +1321,20 @@
 //     }, 1000);
 // }
 // New Program Code For Bom Event - Creating a Clock ------------------>
+var myButton = document.querySelector(".myButton");
+myButton.addEventListener("click", startTime);
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
