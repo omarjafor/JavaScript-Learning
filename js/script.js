2a5720e77726d1c117c0fd9988085717d77773fe
omarjafor <omarjaforchy@gmail.com>
2022-09-26 11:36:17 +0600

    javascript clock

--- js/script.js
+++ js/script.js
@@ -1321,20 +1321,21 @@
 //     }, 1000);
 // }
 // New Program Code For Bom Event - Creating a Clock ------------------>
-var myButton = document.querySelector(".myButton");
-myButton.addEventListener("click", startTime);
-function startTime() {
-    const today = new Date();
-    let h = today.getHours();
-    let m = today.getMinutes();
-    let s = today.getSeconds();
-    m = checkTime(m);
-    s = checkTime(s);
-    document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
-    setInterval(startTime, 1000);
-  }
+// var myButton = document.querySelector(".myButton");
+// myButton.addEventListener("click", startTime);
+// function startTime() {
+//     const today = new Date();
+//     let h = today.getHours();
+//     let m = today.getMinutes();
+//     let s = today.getSeconds();
+//     m = checkTime(m);
+//     s = checkTime(s);
+//     document.getElementById('txt').innerHTML =  h + ":" + m + ":" + s;
+//     setInterval(startTime, 1000);
+//   }
   
-  function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-  }
+//   function checkTime(i) {
+//     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+//     return i;
+//   }
+// New Program Code For Error Handaling in JavaScript ------------------>
