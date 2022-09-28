334d514b2f14572ea190f73ba612d41924c8b1a8
omarjafor <omarjaforchy@gmail.com>
2022-09-28 10:36:26 +0600

    canvas complete

--- js/script.js
+++ js/script.js
@@ -1373,3 +1373,12 @@ ctx.strokeRect(10,10,385,285);
 
 ctx.fillStyle = "green";
 ctx.fillRect(12,12,380,280);
+
+var centerX = c.width / 2;
+var centerY = c.height / 2;
+ctx.beginPath();
+ctx.arc(centerX,centerY,65,0,2*Math.PI,false);
+
+ctx.fillStyle = "red";
+ctx.fill();
+ctx.stroke();
