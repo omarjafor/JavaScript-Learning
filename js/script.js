6bf939bf0c611d8347fd4ec8507d470227e067c7
omarjafor <omarjaforchy@gmail.com>
2022-09-28 11:50:38 +0600

    ES6 start

--- js/script.js
+++ js/script.js
@@ -1364,21 +1364,22 @@
 //     }
 // });
 // New Program Code For Canvas Drawing in JavaScript ------------------>
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+// var c = document.getElementById("myCanvas");
+// var ctx = c.getContext("2d");
 
-ctx.lineWidth=3;
-ctx.strokeStyle = "black";
-ctx.strokeRect(10,10,385,285);
+// ctx.lineWidth=3;
+// ctx.strokeStyle = "black";
+// ctx.strokeRect(10,10,385,285);
 
-ctx.fillStyle = "green";
-ctx.fillRect(12,12,380,280);
+// ctx.fillStyle = "green";
+// ctx.fillRect(12,12,380,280);
 
-var centerX = c.width / 2;
-var centerY = c.height / 2;
-ctx.beginPath();
-ctx.arc(centerX,centerY,65,0,2*Math.PI,false);
+// var centerX = c.width / 2;
+// var centerY = c.height / 2;
+// ctx.beginPath();
+// ctx.arc(centerX,centerY,65,0,2*Math.PI,false);
 
-ctx.fillStyle = "red";
-ctx.fill();
-ctx.stroke();
+// ctx.fillStyle = "red";
+// ctx.fill();
+// ctx.stroke();
+// New Program Code For variable and function in ES6------------------>
