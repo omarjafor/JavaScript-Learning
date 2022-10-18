f29378e8d52f2c755ccc54822551d2cf49cc310a
omarjafor <omarjaforchy@gmail.com>
2022-10-18 23:52:04 +0600

    XMLHttpApi Reqst

--- js/script.js
+++ js/script.js
@@ -1846,6 +1846,12 @@ console.clear();
 const getData = () =>{
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+    xhr.onload = () => {
+        let data = xhr.response;
+        console.log(data);
+    }
+
     xhr.send();
 }
 getData();
