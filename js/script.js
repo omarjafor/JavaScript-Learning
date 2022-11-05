b0f16c9702592f007575b4d740b7980255ced2fb
omarjafor <omarjaforchy@gmail.com>
2022-11-05 21:37:50 +0600

    ES6 Refresher

--- js/script.js
+++ js/script.js
@@ -2074,17 +2074,19 @@
 
 
 // ES6 Refresher ----> Doing some practice again es6
-let x = 5;
-let y = 6;
-console.log(x+y);
-const myName = "jaberjabed";
-console.log(myName);
-function num(name){
-    let num2 = name*2;
-    console.log(num2);
-}
-num(6);
-const numMul = num => console.log(num*2);
-numMul(5);
-const numMul2 = (num,num2) => console.log(num*num2);
-numMul2(5,6);
+// let x = 5;
+// let y = 6;
+// console.log(x+y);
+// const myName = "jaberjabed";
+// console.log(myName);
+// function num(name){
+//     let num2 = name*2;
+//     console.log(num2);
+// }
+// num(6);
+// const numMul = num => console.log(num*2);
+// numMul(5);
+// const numMul2 = (num,num2) => console.log(num*num2);
+// numMul2(5,6);
+import {text, setText} from './myModule.js';
+console.log(text);
