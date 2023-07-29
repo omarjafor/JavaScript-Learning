1bc394e5eee1bac152c95382b203e3785a9e2ed6
omarjafor <omarjaforchy@gmail.com>
2023-07-29 11:59:04 +0600

    string text

---
+++ js/script.js
@@ -0,0 +1,7 @@
+console.log("My Name is Omar Jafor Chy")
+document.write("My name is omar jafor chy")
+
+var firstName = "Omar Jafor "
+var lastName = "Chy Jaber"
+var fullName = firstName + lastName ;
+console.log(fullName)
