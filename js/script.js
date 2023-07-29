0bb6628867594dc3f567de4505f9f09ed50aef41
omarjafor <omarjaforchy@gmail.com>
2023-07-29 12:06:38 +0600

    intfloat

--- js/script.js
+++ js/script.js
@@ -1,7 +1,20 @@
 console.log("My Name is Omar Jafor Chy")
-document.write("My name is omar jafor chy")
+document.write("My name is Omar jafor chy")
 
 var firstName = "Omar Jafor "
 var lastName = "Chy Jaber"
 var fullName = firstName + lastName ;
 console.log(fullName)
+
+var num = 10;
+console.log(typeof(num));
+num = toString(num);
+console.log(typeof(num));
+num = parseInt("1213");
+console.log(typeof(num));
+var num = 10.25;
+console.log(typeof(num));
+num = parseFloat(num);
+console.log(num);
+num = parseInt(num);
+console.log(num);
