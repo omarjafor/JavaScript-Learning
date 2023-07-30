cf6257005fa834c2ca6d055ae71f7810754035e1
omarjafor <omarjaforchy@gmail.com>
2023-07-31 01:50:04 +0600

    task problem

--- js/script.js
+++ js/script.js
@@ -149,13 +149,13 @@ console.log(fruits);
 // for (i=0; i<5; i++){};
 // console.log(i);
 
-// var marks = [ 13, 15, 14, 20, 18];
-// for(var i=0; i<marks.length; i++){
-//  if(marks[i] >=15){
-//   continue;
-//  }
-//  console.log(marks[i]);
-// };
+var marks = [ 13, 15, 14, 20, 18];
+for(var i=0; i<marks.length; i++){
+ if(marks[i] >=15){
+  continue;
+ }
+ console.log(marks[i]);
+};
 
 var i = 5;
 for (i = 5; i < 5; i++) {
