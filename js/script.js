0fdcb7918b819ebff44a5a2c0a4b48eb5f699039
omarjafor <omarjaforchy@gmail.com>
2023-07-31 01:51:05 +0600

    task problem

--- js/script.js
+++ js/script.js
@@ -146,19 +146,19 @@ if(num1==num2 || num1==num3 || num2==num3){
     document.write("The Triangle is Not Isosceles");
 }
 
-var i = 0;
-for (i=0; i<5; i++){};
-console.log(i);
+// var i = 0;
+// for (i=0; i<5; i++){};
+// console.log(i);
 
-var marks = [ 13, 15, 14, 20, 18];
-for(var i=0; i<marks.length; i++){
- if(marks[i] >=15){
-  continue;
- }
- console.log(marks[i]);
-};
+// var marks = [ 13, 15, 14, 20, 18];
+// for(var i=0; i<marks.length; i++){
+//  if(marks[i] >=15){
+//   continue;
+//  }
+//  console.log(marks[i]);
+// };
 
-var i = 5;
-for (i = 5; i < 5; i++) {
- console.log(i);
-}
+// var i = 5;
+// for (i = 5; i < 5; i++) {
+//  console.log(i);
+// }
