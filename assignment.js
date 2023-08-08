e064f8d0a02482cd3c449b987ce030b80ddd00b3
omarjafor <omarjaforchy@gmail.com>
2023-08-08 20:19:49 +0600

    assign 2

--- assignment.js
+++ assignment.js
@@ -1,5 +1,5 @@
 function cubeNumber(number) {
-    if(typeof(number) != 'number'){
+    if(typeof(number) !== 'number'){
         console.log('Please Enter Valid Number');
     }else{
         let cube = number*number*number;
@@ -7,4 +7,17 @@ function cubeNumber(number) {
     }
     
 }
-console.log(cubeNumber('school'));
+console.log(cubeNumber(4));
+
+function matchFinder(string1, string2) {
+    if(typeof(string1) !== 'string' || typeof(string2) !== 'string'){
+        console.log('Please Enter Valid Input');
+    }
+    else if(string1.includes(string2) == true){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(matchFinder('Peter Parker', 'Pet'));
