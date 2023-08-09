a978518b83d2d0225e824d77d1cc24acdda28952
omarjafor <omarjaforchy@gmail.com>
2023-08-09 14:22:34 +0600

    assign cor

--- assignment.js
+++ assignment.js
@@ -1,6 +1,6 @@
 function cubeNumber(number) {
     if(typeof(number) !== 'number'){
-        console.log('Please Enter Valid Number');
+        return 'Please Enter Valid Number';
     }else{
         let cube = number*number*number;
         return cube;
@@ -59,7 +59,7 @@ function canPay(changeArray, totalDue) {
     }
 }
 
-console.log(cubeNumber(4));
+console.log(cubeNumber(''));
 
 console.log(matchFinder('Peter Parker', 'Pet'));
 
