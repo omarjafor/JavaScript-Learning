5780164a5d4e8cf923984ce4f4a3e22f2e72cb42
omarjafor <omarjaforchy@gmail.com>
2023-08-09 11:11:42 +0600

    js practice

--- assignment.js
+++ assignment.js
@@ -21,3 +21,25 @@ function matchFinder(string1, string2) {
     }
 }
 console.log(matchFinder('Peter Parker', 'Pet'));
+
+function sortMaker(arr) {
+    if(arr[0] === arr[1]){
+        return 'equal';
+    }
+    else if(arr[0] >= 0 && arr[1] >= 0){
+        if(arr[1] > arr[0]){
+            let temp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = temp;
+        }
+        return arr;
+    }
+    else{
+        return 'Invalid Input';
+    }
+}
+let arrIs = [0,1]
+console.log(sortMaker(arrIs));
+
+function findAddress(obj) {
+}
