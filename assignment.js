8dadef42a90d6fa1030c61955ace4d7c8b085120
omarjafor <omarjaforchy@gmail.com>
2023-08-10 11:41:06 +0600

    js practice

--- assignment.js
+++ assignment.js
@@ -91,3 +91,11 @@ function compare(a, b) {
    }
    const result = compare(15, "15");
    console.log(result);
+
+function compare(a, b) {
+    if (a.toString() === b) {
+    return true;
+    } else {
+    return false;
+    }
+   }
