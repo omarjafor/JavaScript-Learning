a220f0abb08bebb9c959d1db3222b1cb637df810
omarjafor <omarjaforchy@gmail.com>
2023-08-11 11:25:07 +0600

    Dom Js

--- js/dom.js
+++ js/dom.js
@@ -3,3 +3,9 @@ document.querySelector('#head1').innerHTML="Good Bye Bye";
 
 // document.getElementById('para').innerHTML='Hello I am paragraph';
 document.querySelector('#para').innerHTML='Hello I am paragraph';
+function myMessage(){
+    alert('Hello Switch ON');
+}
+function myMessage2(){
+    alert('Hello Switch OFF');
+}
