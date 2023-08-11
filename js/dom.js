5b1719aa7953873ff8469201904fc46aa390db2d
omarjafor <omarjaforchy@gmail.com>
2023-08-11 11:09:41 +0600

    Dom Js

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,5 @@
 document.querySelector('#head').innerHTML="Good Bye";
 document.querySelector('#head1').innerHTML="Good Bye Bye";
+
+// document.getElementById('para').innerHTML='Hello I am paragraph';
+document.querySelector('#para').innerHTML='Hello I am paragraph';
