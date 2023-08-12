56b29eb8a8ea0767bc0449040522c5e2c7e71e7f
omarjafor <omarjaforchy@gmail.com>
2023-08-12 20:26:34 +0600

    js dom

--- js/dom.js
+++ js/dom.js
@@ -1,29 +1,29 @@
-document.querySelector('#head').innerHTML="Good Bye";
-document.querySelector('#head1').innerHTML="Good Bye Bye";
+// document.querySelector('#head').innerHTML="Good Bye";
+// document.querySelector('#head1').innerHTML="Good Bye Bye";
 
-document.getElementById('para').innerHTML='Hello I am paragraph';
-document.querySelector('#para').innerHTML='Hello I am paragraph';
-function myMessage(){
-    alert('Hello Switch ON');
-}
-function myMessage1(){
-    document.querySelector('#para').innerHTML='Hello I am Paragraph By On Click';
-}
-function myMessage2(){
-    alert('Hello Switch OFF');
-}
+// document.getElementById('para').innerHTML='Hello I am paragraph';
+// document.querySelector('#para').innerHTML='Hello I am paragraph';
+// function myMessage(){
+//     alert('Hello Switch ON');
+// }
+// function myMessage1(){
+//     document.querySelector('#para').innerHTML='Hello I am Paragraph By On Click';
+// }
+// function myMessage2(){
+//     alert('Hello Switch OFF');
+// }
 
-const sections = document.querySelectorAll('h1');
-for(const sec of sections){
-    sec.style.border = '2px solid red';
-    sec.style.margin = '15px';
-    sec.style.padding = '5px';
-    sec.style.backgroundColor = 'blue';
-}
+// const sections = document.querySelectorAll('h1');
+// for(const sec of sections){
+//     sec.style.border = '2px solid red';
+//     sec.style.margin = '15px';
+//     sec.style.padding = '5px';
+//     sec.style.backgroundColor = 'blue';
+// }
 
 const lisection = document.getElementById('lisection');
 lisection.classList.add('newClass');
 lisection.classList.add('newClass2');
 lisection.classList.add('newClass3');
 lisection.classList.add('newClass4');
-lisection.classList.remove('newClass3');
+lisection.classList.remove('newClass');
