264df49fc7774de9c25a28723356cee3abd1cf00
omarjafor <omarjaforchy@gmail.com>
2023-08-12 18:43:24 +0600

    js dom

--- js/dom.js
+++ js/dom.js
@@ -1,8 +1,8 @@
 document.querySelector('#head').innerHTML="Good Bye";
 document.querySelector('#head1').innerHTML="Good Bye Bye";
 
-// document.getElementById('para').innerHTML='Hello I am paragraph';
-// document.querySelector('#para').innerHTML='Hello I am paragraph';
+document.getElementById('para').innerHTML='Hello I am paragraph';
+document.querySelector('#para').innerHTML='Hello I am paragraph';
 function myMessage(){
     alert('Hello Switch ON');
 }
