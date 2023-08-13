e271f800275668164c28e9b9655ec3700c2ba7fc
omarjafor <omarjaforchy@gmail.com>
2023-08-13 21:16:16 +0600

    delegate

--- js/dom.js
+++ js/dom.js
@@ -3,13 +3,13 @@
 
 // document.getElementById('para').innerHTML='Hello I am paragraph';
 // document.querySelector('#para').innerHTML='Hello I am paragraph';
-function myMessage(){
+function myMessage() {
     alert('Hello Switch ON');
 }
 // function myMessage1(){
 //     document.querySelector('#para').innerHTML='Hello I am Paragraph By On Click';
 // }
-function myMessage2(){
+function myMessage2() {
     alert('Hello Switch OFF');
 }
 
@@ -28,20 +28,20 @@ lisection.classList.add('newClass3');
 lisection.classList.add('newClass4');
 lisection.classList.remove('newClass');
 
-function textChange(){
+function textChange() {
     let sectionH2 = document.getElementById('sectionH2');
-    sectionH2.innerHTML='Cricket List Football';
+    sectionH2.innerHTML = 'Cricket List Football';
 }
 
-document.getElementById('changeButton').addEventListener('click', function(){
+document.getElementById('changeButton').addEventListener('click', function () {
     const headText = document.getElementById('headtext');
     headText.innerText = 'Changed The Heading Now By OmarJafor';
 });
 
-document.getElementById('btn-upd').addEventListener('click', function(){
+document.getElementById('btn-upd').addEventListener('click', function () {
     const inputField = document.getElementById('inputField');
     const inputText = inputField.value;
-    
+
     const headText = document.getElementById('headtext');
     headText.innerText = inputText;
     inputField.value = '';
@@ -67,31 +67,38 @@ document.getElementById('btn-upd').addEventListener('click', function(){
 //     console.log('btn clicked');
 // })
 
-document.getElementById('inputBox').addEventListener('keyup', function(event){
+document.getElementById('inputBox').addEventListener('keyup', function (event) {
     const text = event.target.value;
     const deleteButton = document.getElementById('btn-delete');
-    if(text.toLowerCase() == 'delete'){
+    if (text.toLowerCase() == 'delete') {
         deleteButton.removeAttribute('disabled')
-        
-    }else{
+
+    } else {
         deleteButton.setAttribute('disabled', true)
     }
 })
-document.getElementById('btn-delete').addEventListener('click', function(){
+document.getElementById('btn-delete').addEventListener('click', function () {
     const secret = document.getElementById('secretinfo');
     secret.style.display = 'none';
     const inputBox = document.getElementById('inputBox');
     inputBox.value = '';
 })
 
-document.getElementById('li-2').addEventListener('click', function(event){
-    console.log('list 2 clicked');
-    event.stopPropagation();
-    event.stopImmediatePropagation();
-})
-document.getElementById('list-ul').addEventListener('click', function(){
-    console.log('UL clicked');
-})
-document.getElementById('sec-li').addEventListener('click', function(){
-    console.log('FULL Section clicked');
-})
+// document.getElementById('li-2').addEventListener('click', function(event){
+//     console.log('list 2 clicked');
+//     event.stopPropagation();
+//     event.stopImmediatePropagation();
+// })
+// document.getElementById('list-ul').addEventListener('click', function(){
+//     console.log('UL clicked');
+// })
+// document.getElementById('sec-li').addEventListener('click', function(){
+//     console.log('FULL Section clicked');
+// })
+
+const items = document.getElementsByClassName('item');
+for (const item of items) {
+    item.addEventListener('click', function (event) {
+        event.target.parentNode.removeChild(event.target);
+    })
+}
