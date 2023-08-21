55914bacb5f5728a395a7c04a2213b78277737fc
omarjafor <omarjaforchy@gmail.com>
2023-08-21 19:22:08 +0600

    validation

--- js/date.js
+++ js/date.js
@@ -8,3 +8,10 @@ if(date1.getTime() < date2.getTime()){
 }else{
     console.log('March is not befor december');
 };
+
+const number = 25;
+if(typeof number === 'number'){
+    console.log('Value is a number');
+}else{
+    console.log('Value is not a number');
+}
