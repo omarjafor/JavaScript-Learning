58b36d82d9340d219ebb798f5c4ee6a966a589d2
omarjafor <omarjaforchy@gmail.com>
2023-08-21 19:18:09 +0600

    date object

--- js/date.js
+++ js/date.js
@@ -3,6 +3,8 @@ console.log(today);
 
 const date1 = new Date ('1971-03-26');
 const date2 = new Date ('1971-12-16');
-if(date1>date2){
+if(date1.getTime() > date2.getTime()){
     console.log('March Befor December');
+}else{
+    console.log('March is not befor december');
 };
