e09802ee826325960a4d58e81c3fa2ddd8daece6
omarjafor <omarjaforchy@gmail.com>
2024-01-07 12:11:28 +0600

    javascript practice

---
+++ js/Practice.js
@@ -0,0 +1,15 @@
+// challenge #1 Print numbers from 1 to 10
+// for(let i = 1; i<11; i++) {
+//     console.log(i);
+//     document.write(i + '<br>');
+// }
+
+// challenge #2: Print the odd numbers less than 100 
+// for (let i = 1; i < 100; i+=2) {
+//     document.write(i + '<br>');
+// }
+
+// challenge #3: Print the multiplication table with 7
+// for(let i=1; i<11; i++){
+//     console.log(`7 * ${i} = ${7*i}`);
+// }
