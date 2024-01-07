ef1b2cf59f16976be2969a0e37dff4d9b756e228
omarjafor <omarjaforchy@gmail.com>
2024-01-07 12:37:12 +0600

    javascript challenge

--- js/Practice.js
+++ js/Practice.js
@@ -25,3 +25,10 @@
 //     multiTable(i);
 //     console.log('\n');
 // }
+
+// challenge #5: Calculate the sum of numbers from 1 to 10
+let sum = 0;
+for(let i = 1; i<11; i++){
+    sum += i;
+}
+console.log(sum);
