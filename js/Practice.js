fde702c3d928f813328bc581bdaecdf66b7551a4
omarjafor <omarjaforchy@gmail.com>
2024-01-07 12:40:39 +0600

    javascript challenge 6

--- js/Practice.js
+++ js/Practice.js
@@ -27,8 +27,15 @@
 // }
 
 // challenge #5: Calculate the sum of numbers from 1 to 10
-let sum = 0;
+// let sum = 0;
+// for(let i = 1; i<11; i++){
+//     sum += i;
+// }
+// console.log(sum);
+
+// challenge #6: Calculate 10!
+let prod = 1;
 for(let i = 1; i<11; i++){
-    sum += i;
+    prod *= i;
 }
-console.log(sum);
+console.log(prod);
