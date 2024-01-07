c30e6759728ce061b45f9f223ee4a0438730ea37
omarjafor <omarjaforchy@gmail.com>
2024-01-07 12:33:26 +0600

    javascript challenge

--- js/Practice.js
+++ js/Practice.js
@@ -4,7 +4,7 @@
 //     document.write(i + '<br>');
 // }
 
-// challenge #2: Print the odd numbers less than 100 
+// challenge #2: Print the odd numbers less than 100
 // for (let i = 1; i < 100; i+=2) {
 //     document.write(i + '<br>');
 // }
@@ -12,4 +12,16 @@
 // challenge #3: Print the multiplication table with 7
 // for(let i=1; i<11; i++){
 //     console.log(`7 * ${i} = ${7*i}`);
+// }
+
+// challenge #4: Print all the multiplication tables with numbers from 1 to 10
+// function multiTable(num){
+//     for(let i=1; i<11; i++){
+//         console.log(`${num} * ${i} = ${num * i}`);
+//     }
+// }
+
+// for(let i=1; i<11; i++){
+//     multiTable(i);
+//     console.log('\n');
 // }
