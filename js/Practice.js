fe9ed42820093bb990338476f43ac1e663e98f23
omarjafor <omarjaforchy@gmail.com>
2024-01-07 12:52:01 +0600

    javascript challenge 7

--- js/Practice.js
+++ js/Practice.js
@@ -39,3 +39,12 @@
 //     prod *= i;
 // }
 // console.log(prod);
+
+// challenge #7: Calculate the sum of odd numbers greater than 10 and less than 30
+let sum = 0;
+for(let i = 11; i<30; i++){
+    if(i%2==1){
+        sum += i;
+    }
+}
+console.log(sum);
